f2f2c11eaaea0894353aa5d1f204bd308a0eb682
dataisland <dataisland@outlook.com>
2019-04-08 21:24:30 +0800

    auth

---
+++ ext/config.py
@@ -0,0 +1,15 @@
+import os.path
+import tornado.options
+
+global_settings = {
+    "default" : 8000,
+    "help" : "debug version"
+}
+
+app_settings = {
+    "template_path" : os.path.join(os.path.dirname(__file__), "template"),
+    "static_path"   : os.path.join(os.path.dirname(__file__), "static"),
+    "cookie_secret" : os.urandom(64),
+    "xsrf_cookies"  : True,
+    "login_url"     : "/login"
+}
