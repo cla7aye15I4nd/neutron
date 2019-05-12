335e786a0d92800bf14e2ffa9f2fbde3ee698896
ulitaig <44859055+ulitaig@users.noreply.github.com>
2019-05-12 20:37:16 +0800

    Add files via upload

--- ext/base.py
+++ ext/base.py
@@ -5,3 +5,7 @@ class BaseHandler(tornado.web.RequestHandler):
         return self.get_secure_cookie("user")
     def set_current_user(self, idx):
         self.set_secure_cookie("user", str(idx), httponly=True)
+    def get(self):
+        self.write_error(404)
+    def write_error(self, status_code, **kwargs):
+        self.render('error.html', title='error', user = self.current_user,stcode=status_code)
