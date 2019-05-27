ebb3e3f0c645e7eef8aaeab7b854ce966404b64b
dataisland <dataisland@outlook.com>
2019-05-27 22:51:35 +0800

    data

---
+++ data/2/generate_all.py
@@ -0,0 +1,10 @@
+import os
+os.system("rm all.in")
+for i in range(1,100):
+  if os.path.exists(str(i)+'.in'):
+    command =  "cat {}.in >> all.in && echo \"\\n\" >> all.in ".format(str(i))
+    print(command)
+    os.system(command)
+  else:
+    break
+
