4d3df3fd06b60644895066135e771705d5ff44d6
dataisland <dataisland@outlook.com>
2019-05-28 15:08:24 +0800

    data error

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,6 @@ trainData temp;
 
 saleSystem sys;
 int main() {
-    //fprintf(stderr, "%d\n", sizeof(temp));
-    double begin = clock();
-    //freopen("all.in", "r", stdin);
-	//freopen("all.out", "w", stdout);
-	sys.processing();
-    //fprintf(stderr, "%f\n", (clock() - begin) / CLOCKS_PER_SEC);
-	return 0;
+    sys.processing();
+    return 0;
 }
