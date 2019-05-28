4c06f08d754f33abfb1d2d61fc8eb92adf9a5fb6
dataisland <dataisland@outlook.com>
2019-05-29 01:35:43 +0800

    @

--- src/saleSystem.hpp
+++ src/saleSystem.hpp
@@ -33,8 +33,7 @@ public:
             if (scanf("%s", order.ch) == -1) break;
             int p = 0;
             while (p < orderN && orders[p] != order) p++;
-            switch (p)
-                {
+            switch (p) {
                 case 0:user_system.append(); break;
                 case 1:user_system.login(); break;
                 case 2:user_system.query_profile(); break;
